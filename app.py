e720846e2446c321e61c436c4cab013422d651a3
Joshua <jhellst@gmail.com>
2023-08-24 15:48:37 -0700

    Added session data to store question count and responses

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-question_number = 0
-responses = []
+app.config['SECRET_KEY'] = "secret-key-yes-very-secret"
+
 
 @app.get("/")
 def homepage():
+
+    session["question_number"] = 0
+    session["responses"] = []
 
     return render_template("survey_start.html", survey_title=survey.title, survey_instructions=survey.instructions)
 
@@ -20,14 +23,28 @@ def homepage():
 @app.post("/answer_question")
 def answer_question():
 
-    if question_number < len(survey.questions)-1:
-        return redirect(f"/question/{question_number}")
+    cur_question_number = session["question_number"]
 
+    cur_answer = request.form.get("answer")
 
-    return redirect("completion.html")
+    if cur_answer:
+        session["responses"].append(cur_answer)
+    print("Cur Answer", cur_answer)
+
+    if cur_question_number < len(survey.questions):
+        session["question_number"] += 1
+        return redirect(f"/question/{cur_question_number}")
+
+    return redirect("/completion")
 
 
 @app.get("/question/<int:question_number>")
 def get_first_question(question_number):
 
     return render_template("question.html", question=survey.questions[question_number])
+
+
+@app.get("/completion")
+def get_completion_message():
+
+    return render_template("completion.html", responses=session["responses"])

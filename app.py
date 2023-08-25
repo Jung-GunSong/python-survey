253941b5d128dfd2ed7d0e02a08c9da4911ee4c5
Joshua <jhellst@gmail.com>
2023-08-24 17:17:46 -0700

    Completed survey with buttons and info, generated completion page with info

--- app.py
+++ app.py
@@ -13,25 +13,27 @@ app.config['SECRET_KEY'] = "secret-key-yes-very-secret"
 
 @app.get("/")
 def homepage():
+    """Starting page"""
 
+    # Move these to answer_question(), we want to modify session data in a post
     session["question_number"] = 0
     session["responses"] = []
 
     return render_template("survey_start.html", survey_title=survey.title, survey_instructions=survey.instructions)
+    # TODO: Just give the survey object for jinja to access
 
-
+# Split this up to have 1 to start survey and 1 to answer the question
 @app.post("/answer_question")
 def answer_question():
+    """Logic for answering a question"""
 
-    cur_question_number = session["question_number"]
-
+    cur_question_number = session["question_number"] # This is not needed, we could just extract this from len(responses)
     cur_answer = request.form.get("answer")
     cur_responses = session["responses"]
-    # print("sessions responses are", session["responses"])
+
     if cur_answer:
         cur_responses.append(cur_answer)
-    # print("Cur Answer", cur_answer, type (cur_answer))
-    # print("sessions responses after append", session["responses"])
+
     session["responses"] = cur_responses
     if cur_question_number < len(survey.questions):
         session["question_number"] += 1
@@ -41,12 +43,18 @@ def answer_question():
 
 
 @app.get("/question/<int:question_number>")
-def get_first_question(question_number):
-    # print("sessions responses after redirect", session["responses"])
-    return render_template("question.html", question=survey.questions[question_number])
+def get_first_question(question_number): # Rename this
+    """Loads next question"""
+    questions = survey.questions
+    return render_template("question.html", question=questions[question_number])
 
 
 @app.get("/completion")
 def get_completion_message():
+    """Loads completion message with results after last question"""
     # print("sessions responses before completion", session["responses"])
-    return render_template("completion.html", responses = session["responses"])
+    return render_template(
+        "completion.html",
+        questions = survey.questions,
+        responses = session["responses"]
+    )
